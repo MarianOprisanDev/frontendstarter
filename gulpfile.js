4f71eceb787ed455211bbc64e92f1a8a90e00758
lvl <marian.razvan.oprisan@gmail.com>
2017-09-26 23:06:52 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
+
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+
+// move all html to dist/ minified
+gulp.task('copyHtml', () => {
+    gulp.src('src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'));
+});
+
+// minify js and copy to dist/js/
+gulp.task('minjs', () => {
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// compile sass, minify sass css files and copy them to dist/
+gulp.task('styles', () => {
+    gulp.src('src/sass/*.scss')
+        // if sass() returns an error, we must log it ourselves
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+    gulp.src('src/sass/*.scss')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// maintenance: run all tasks
+gulp.task('main', ['copyHtml', 'minjs', 'sass']);
+
+// showing instructions
+gulp.task('default', () => {
+    console.log('If the dist folder is missing from the project, run gulp main to set it up.');
+    console.log('Other gulp tasks: copyHtml, minjs, styles.');
+    console.log('See gulpfile for more info on each task.')
+})
+
+// watch for file changes
+gulp.task('watch', () => {
+    gulp.watch('src/*.html', ['copyHtml']);
+    gulp.watch('src/sass/*.scss', ['styles']);
+    gulp.watch('src/js/*.js', ['minjs']);
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+# This is a starter file for front-end projects
+## Using Sass 
+***
+
+### Requirements:
+* node.js
+* npm
+
+### Instructions:
+
+* Open a terminal inside the project folder, where package.json is.
+* Run npm i to install development dependencies.
+* Run gulp main to create and populate the dist folder, or gulp to get instructions on using the tasks inside the gulpfile.
+* Run gulp watch to have gulp watch for file changes and update the dist folder accordingly.
+
+### Observations
+* Edit code only inside the src folder
+* All html, css and js code inside the dist folder will be minified
